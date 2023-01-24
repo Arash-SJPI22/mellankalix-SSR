96684acb727cc66c886bee16f99e6f7eec576a46
Arash-SJPI22 <arash.eblaghian@lerniastudent.se>
2023-01-24 10:57:18 +0100

    Initialized EJS and views, partials and index.ejs

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
 app.use("/", express.static("./"));
 
 app.listen(5080);
