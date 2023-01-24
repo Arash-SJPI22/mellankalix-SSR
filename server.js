c7ee2a5b3da21ecbda88cc86f781817fa4c17e39
Arash-SJPI22 <arash.eblaghian@lerniastudent.se>
2023-01-24 11:24:08 +0100

    EJS movies (needs fixing when new API is implemented)

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get(["/", "/about", "/barAndBistro", "/comingPremieres", "/giftcertificate"], (req, res) => {
+app.get(["/", "/about", "/barAndBistro", "/comingPremieres", "/giftcertificate", "/movie"], (req, res) => {
 	if (req.url == "/") {
         res.render("index");
     } else {
